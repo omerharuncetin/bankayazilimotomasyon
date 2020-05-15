84780b6a98c7f873be929a28c2393acc69c0e592
Fatih <fatih.hazir@bakircay.edu.tr>
2020-05-15 16:58:02 +0300

    Ufak test

---
+++ bankayazilimotomasyon/Test.cpp
@@ -0,0 +1,10 @@
+#include "BireyselMusteri.h"
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    BireyselMusteri bilgiler("fatih", "hazr", "55", "ff", "44", 4);
+    TicariMusteri Fatih("454545", "4545", bilgiler);
+    
+}
